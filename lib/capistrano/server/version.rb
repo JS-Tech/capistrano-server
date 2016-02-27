220c6123ebed1d30412acb86b8dbbb91aedeff45
khcr <kocher.ke@gmail.com>
2016-02-27 15:13:20 +0100

    version

--- lib/capistrano/server/version.rb
+++ lib/capistrano/server/version.rb
@@ -3,3 +3,9 @@ module Capistrano
     VERSION = "0.5.1"
   end
 end
+
+resj ALL=(ALL) NOPASSWD: /bin/ln -* /* /etc/nginx/sites-enabled/*
+resj ALL=(ALL) NOPASSWD: /sbin/start puma app=*, /sbin/stop puma app=*, /sbin/restart puma app=*, /sbin/status puma app=*
+resj ALL=(ALL) NOPASSWD: /sbin/start nginx, /sbin/stop nginx, /sbin/restart nginx, /sbin/reload nginx
+resj ALL=(ALL) NOPASSWD: /bin/ln -* /* /etc/init/puma.conf, /bin/ln -* /* /etc/init/puma-manager.conf, /bin/ln -* /* /home/resj/apps/*, /bin/ln -* /* /etc/puma.conf
+resj ALL=(ALL) NOPASSWD: /sbin/initctl reload-configuration
